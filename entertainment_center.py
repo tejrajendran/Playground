7f995e850ff602672bdf7f785f2d7ac62a478968
tejrajendran <tejashwini.rajendran@ge.com>
2017-06-13 11:45:57 -0700

    movie viewer

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,6 +2,7 @@
 # keep class definitions in their own file
 
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story about a boy and his toys that come to life.",
@@ -13,3 +14,16 @@ avatar = media.Movie("Avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
+batman = media.Movie("The Dark Knight",
+                     "A crime fighter protects Gotham City.",
+                     "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+                     "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+
+nemo = media.Movie("Finding Nemo",
+                   "A fathers search for his son through the ocean.",
+                   "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
+                   "https://www.youtube.com/watch?v=wZdpNglLbt8")
+
+
+movies = [toy_story,avatar,batman,nemo]
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -11,7 +11,7 @@ class Movie():
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_url = trailer
+        self.trailer_youtube_url = trailer
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
