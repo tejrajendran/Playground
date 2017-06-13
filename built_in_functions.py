ef18a4a710319c22ec91209bfec13f8592ec40d2
tejrajendran <tejashwini.rajendran@ge.com>
2017-06-13 09:44:36 -0700

    playing with build in functions

---
+++ built_in_functions.py
@@ -0,0 +1,11 @@
+def sort_my_array(my_arr):
+    for item in my_arr:
+        print item,
+
+    my_arr = sorted(my_arr)
+    print('\n')
+
+    for item in my_arr:
+        print item,
+
+sort_my_array(['z', 'g', 'a', 'r', 'u', 'o', 'b'])
