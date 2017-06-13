8586eb23adcb4e892a2a20f1ad85668f480bc24e
tejrajendran <tejashwini.rajendran@ge.com>
2017-06-13 11:35:18 -0700

    movie class

---
+++ entertainment_center.py
@@ -0,0 +1,15 @@
+# use the class we defined in the media file
+# keep class definitions in their own file
+
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story about a boy and his toys that come to life.",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet.",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+
