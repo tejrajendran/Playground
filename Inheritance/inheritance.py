6154d790479416789074fd1388b10633f6ab2ae1
tejrajendran <tejashwini.rajendran@ge.com>
2017-06-13 14:20:21 -0700

    update folder structure

---
+++ Inheritance/inheritance.py
@@ -0,0 +1,10 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+
+class Child :
+
+
+billy_cyrus = Parent("Cyrus","blue")
