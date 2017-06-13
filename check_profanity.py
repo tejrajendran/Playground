f86cda8f0cbdf250d872b7c47feddce69cdc87fe
tejrajendran <tejashwini.rajendran@ge.com>
2017-06-13 09:28:42 -0700

    Program to open and read text file and scan for curse words

---
+++ check_profanity.py
@@ -0,0 +1,6 @@
+def read_text():
+    quotes = open("/Users/212571508/Documents/workspace/full_stack_web_developer/Playground/movie_quotes.txt")
+    contents_of_file = quotes.read();
+    print(contents_of_file)
+
+read_text()
